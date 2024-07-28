339f6de3aceeef92b1bbcc11cdcf053538066934
Giscard Biamby <giscard@gmail.com>
2024-07-28 23:01:48 +0000

    Making the training work locally

--- projects/ViTDet/configs/vit-B_det-rcnn-xview.py
+++ projects/ViTDet/configs/vit-B_det-rcnn-xview.py
@@ -1,29 +1,27 @@
 _base_ = [
-    '../../../configs/_base_/models/mask-rcnn_r50_fpn.py',
+    "../../../configs/_base_/models/mask-rcnn_r50_fpn.py",
     # '/home/mlavery/scalemae_docker/mmdetection/projects/ViTDINO-FSDP/visualize_training.py',
-    '../../ViTDINO-FSDP/xview_dataset.py',
+    "../../ViTDINO-FSDP/xview_dataset.py",
 ]
 
-from mmdet.models import ViTMAE
-from functools import partial
-from torch.nn import LayerNorm as LN
-from mmengine.model.weight_init import PretrainedInit
+# from mmdet.models import ViTMAE
+# from functools import partial
+# from torch.nn import LayerNorm as LN
+# from mmengine.model.weight_init import PretrainedInit
 
-custom_imports = dict(imports=['projects.ViTDet.vitdet'])
+custom_imports = dict(imports=["projects.ViTDet.vitdet"])
 
-backbone_norm_cfg = dict(type='LN', requires_grad=True, eps=1e-6)
-norm_cfg = dict(type='LN2d', requires_grad=True)
+backbone_norm_cfg = dict(type="LN", requires_grad=True, eps=1e-6)
+norm_cfg = dict(type="LN2d", requires_grad=True)
 image_size = (512, 512)
-batch_augments = [
-    dict(type='BatchFixedSizePad', size=image_size, pad_mask=True)
-]
+batch_augments = [dict(type="BatchFixedSizePad", size=image_size, pad_mask=True)]
 
 # model settings
 model = dict(
     data_preprocessor=dict(pad_size_divisor=32, batch_augments=batch_augments),
     backbone=dict(
         _delete_=True,
-        type='ViT',
+        type="ViT",
         img_size=1024,
         patch_size=16,
         embed_dim=768,
@@ -46,45 +44,48 @@ model = dict(
         ],
         use_rel_pos=True,
         init_cfg=dict(
-            type='Pretrained', checkpoint='/home/mlavery/scalemae_docker/weights/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294.pth')),
+            type="Pretrained",
+            checkpoint="/home/mlavery/scalemae_docker/weights/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294.pth",
+        ),
+    ),
     neck=dict(
         _delete_=True,
-        type='SimpleFPN',
+        type="SimpleFPN",
         backbone_channel=768,
         in_channels=[192, 384, 768, 768],
         out_channels=256,
         num_outs=5,
-        norm_cfg=norm_cfg),
+        norm_cfg=norm_cfg,
+    ),
     rpn_head=dict(
-        num_convs=2, 
+        num_convs=2,
     ),
     roi_head=dict(
         bbox_head=dict(
-            type='Shared4Conv1FCBBoxHead',
+            type="Shared4Conv1FCBBoxHead",
             conv_out_channels=256,
             norm_cfg=norm_cfg,
-            num_classes=60
+            num_classes=60,
         ),
-        mask_head=dict(
-            num_classes=60
-        )
+        mask_head=dict(num_classes=60),
     ),
 )
 
 
 optim_wrapper = dict(
-    type='AmpOptimWrapper',
-    constructor='LayerDecayOptimizerConstructor',
+    type="AmpOptimWrapper",
+    constructor="LayerDecayOptimizerConstructor",
     paramwise_cfg={
-        'decay_rate': 0.7,
-        'decay_type': 'layer_wise',
-        'num_layers': 12,
+        "decay_rate": 0.7,
+        "decay_type": "layer_wise",
+        "num_layers": 12,
     },
     optimizer=dict(
-        type='AdamW',
+        type="AdamW",
         lr=0.0001,
         betas=(0.9, 0.999),
         weight_decay=0.1,
-    ))
-custom_hooks = [dict(type='Fp16CompresssionHook')]
+    ),
+)
+custom_hooks = [dict(type="Fp16CompresssionHook")]
 auto_scale_lr = dict(base_batch_size=64)
